4f3ecdc9cfd9b4449a16626aa16603258beafd61
hilberth01 <36186709+hilberth01@users.noreply.github.com>
2018-02-20 22:31:38 +0800

    test

--- Pdc/Pdc/WindowsFormsApplication1/Form1.Designer.cs
+++ Pdc/Pdc/WindowsFormsApplication1/Form1.Designer.cs
@@ -60,7 +60,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(601, 537);
+            this.tabPage2.Size = new System.Drawing.Size(805, 537);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Maintenance";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -73,6 +73,7 @@
             this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Post Dated Cheque Management";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
